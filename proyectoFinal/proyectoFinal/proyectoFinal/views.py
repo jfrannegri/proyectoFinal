e01c2b68c53aa5d77abf528a52dd2164e458200e
Juan Francisco Negri <jfrancisco.negri@gmail.com>
2021-12-10 23:35:56 +0100

    Primera versión

---
+++ proyectoFinal/proyectoFinal/proyectoFinal/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.http.response import HttpResponsePermanentRedirect
+from datetime import datetime
+from django.template import Template, Context
+
+def saludo(request):
+    return HttpResponse("Proyecto final de CODER")
+
+def segundaVista(request):
+    return HttpResponse("Segunda vista-----Proyecto final de CODER")
+
+def dia(request):
+    variable = datetime.now()
+    return HttpResponse(f"Hoy es el día<br> {variable}")
+
+def apellido(request, ape):
+    fecha = datetime.now()
+    return HttpResponse(f"el profe de apellido {ape} nos dío clase el día {fecha}")
+
+def probandoTemplate(request):
+    miHTML = open("C:/Users/Juan Francisco Negri/Desktop/proyectoFinal/proyectoFinal/proyectoFinal/plantillas/template1.html")
+
+    plantilla = Template(miHTML.read())
+
+    miHTML.close()
+
+    miContexto = Context()
+
+    documento = plantilla.render(miContexto)
+
+    return HttpResponse(documento)
+
